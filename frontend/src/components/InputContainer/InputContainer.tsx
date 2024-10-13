473a96e2a2cbe300f1061eaaa7cf314d7141470d
Shane <71288697+shanebishop1@users.noreply.github.com>
2024-10-13 16:51:34 -0700

    add detail to skeleton

--- frontend/src/components/InputContainer/InputContainer.tsx
+++ frontend/src/components/InputContainer/InputContainer.tsx
@@ -1,7 +1,12 @@
 import styles from './InputContainer.module.css';
 
-function InputContainer() {
-  return <div className={styles.inputContainer}>Input</div>;
-}
+const InputContainer = () => {
+  return (
+    <div className={styles.inputVerticalContainer}>
+      <div className={styles.inputTextContainer}> </div>
+      <div className={styles.inputOptionsContainer}> </div>
+    </div>
+  );
+};
 
 export default InputContainer;
