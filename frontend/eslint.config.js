fc069d38cffe4879a58899e40c741a304b18b0b8
Shane <71288697+shanebishop1@users.noreply.github.com>
2024-10-13 16:29:08 -0700

    update eslint settings

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -23,6 +23,8 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
-  }
+  },
 );
