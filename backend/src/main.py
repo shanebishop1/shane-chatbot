98cff0c7f0bb500102cce94930e0730a88028f1e
Shane <71288697+shanebishop1@users.noreply.github.com>
2024-10-14 15:32:27 -0700

    continue to refactor and make llm response dynamic based on the user context/message

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,8 +3,11 @@ from pprint import pprint
 import time
 import random
 from fastapi import FastAPI
+from typing import List
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+from .llm.llm import get_llm_response
+from .types.types import Message
 
 load_dotenv()
 frontend_url = os.getenv("FRONTEND_URL")
@@ -21,27 +24,24 @@ if frontend_url:
     )
 
 
-@app.get("/")
-def read_root():
+@app.get("/chat/{context_id}", response_model=List[Message])
+def get_chat_thread():
+    return messages
+
+
+@app.post("/chat", response_model=Message)
+def send_message(newMessage: Message):
+    llm_response = get_llm_response(
+        f"Context is {newMessage.context}:\n {newMessage.text}"
+    )
+
     # Get time in milliseconds to match how it's handled in the frontend
     timestamp = int(time.time() * 1000)
-    llm_response = get_llm_response("Tell me something cool in less than 20 words.")
     llm_response_message = llm_response.content
     return {
-        "id": timestamp - random.random(),
+        "id": timestamp - round(random.random() * 10),
         "sender": "chatgpt",
         "text": llm_response_message,
+        "context": newMessage.context,
         "timestamp": timestamp,
     }
-
-
-def get_llm_response(message):
-    client = OpenAI()
-    completion = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": message},
-        ],
-    )
-    return completion.choices[0].message
