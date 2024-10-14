566613b94b5d130f987e370ef654f7b1442cd6c1
Shane <71288697+shanebishop1@users.noreply.github.com>
2024-10-14 13:40:07 -0700

    write basic functionality to retrieve a chatgpt message

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,8 @@
 import os
+from pprint import pprint
+from openai import OpenAI
+import time
+import random
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
@@ -20,4 +24,25 @@ if frontend_url:
 
 @app.get("/")
 def read_root():
-    return {"message": "environment setup"}
+    # Get time in milliseconds to match how it's handled in the frontend
+    timestamp = int(time.time() * 1000)
+    llm_response = get_llm_response("Tell me something cool in less than 20 words.")
+    llm_response_message = llm_response.content
+    return {
+        "id": timestamp - random.random(),
+        "sender": "chatgpt",
+        "text": llm_response_message,
+        "timestamp": timestamp,
+    }
+
+
+def get_llm_response(message):
+    client = OpenAI()
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message},
+        ],
+    )
+    return completion.choices[0].message
