3c4414387fb4d11f43512ef6e471983f84c2a6c1
Shane <71288697+shanebishop1@users.noreply.github.com>
2024-10-14 21:38:08 -0700

    attempt to fix heroku db connection issue

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -11,6 +11,8 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable not set")
+elif DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
