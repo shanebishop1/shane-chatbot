21d6675cdc3180b3018934cb94c229ba3eb24c48
Shane <71288697+shanebishop1@users.noreply.github.com>
2024-10-16 23:43:31 -0700

    refactor llm code

--- backend/src/llm/llm.py
+++ backend/src/llm/llm.py
@@ -1,29 +1,36 @@
+from typing import Dict, List
 from openai import OpenAI
-from src.database.database import query_chat_thread_by_context
+from openai.types.chat import ChatCompletion
+
+from src.types.types import Message
 
 
-def get_llm_response(message, context, database):
-    llm_context = [
+def get_llm_response(context: str, context_history: List[Message]) -> str:
+    llm_context: List[Dict[str, str]] = [
         {
             "role": "system",
-            "content": f"The overall context of this converstaion is: {context}",
+            "content": f"You are an AI chatbot named Ava, made by a company called Artisan. You help with sales and marketing. The overall context of this conversation will be: {context}",
         },
     ]
-    thread = query_chat_thread_by_context(context, database)
-    llm_context += convert_thread_to_llm_history(context, thread)
-    client = OpenAI()
-    completion = client.chat.completions.create(
+    llm_context += convert_thread_to_llm_history(context_history)
+    client: OpenAI = OpenAI()
+    completion: ChatCompletion = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=llm_context,  # type: ignore
     )
-    return completion.choices[0].message
+    llm_response: str | None = completion.choices[0].message.content
+    if llm_response is None:
+        raise ValueError("LLM response is empty")
+    return llm_response
 
 
-def convert_thread_to_llm_history(context, thread):
-    return [convert_message_to_llm_interaction(message) for message in thread]
+def convert_thread_to_llm_history(
+    context_history: List[Message],
+) -> List[Dict[str, str]]:
+    return [convert_message_to_llm_interaction(message) for message in context_history]
 
 
-def convert_message_to_llm_interaction(message):
+def convert_message_to_llm_interaction(message: Message) -> Dict[str, str]:
     return {
         "role": "user" if message.sender == "user" else "assistant",
         "content": message.text,
